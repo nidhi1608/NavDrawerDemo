c1cbf77511dc98b9e21d7f286c21ed72eed52291
nidhi1608 <nidhi.1608@gmail.com>
2014-09-18 02:14:50 -0700

    Initial Commit

---
+++ src/com/codepath/navdrawerdemo/FirstFragment.java
@@ -0,0 +1,7 @@
+package com.codepath.navdrawerdemo;
+
+import android.support.v4.app.Fragment;
+
+public class FirstFragment extends Fragment {
+
+}
---
+++ src/com/codepath/navdrawerdemo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.codepath.navdrawerdemo;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MainActivity extends FragmentActivity {
+    private FragmentNavigationDrawer dlDrawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Find our drawer view
+        dlDrawer = (FragmentNavigationDrawer) findViewById(R.id.drawer_layout);
+        // Setup drawer view
+        dlDrawer.setupDrawerConfiguration((ListView) findViewById(R.id.lvDrawer), 
+                     R.layout.drawer_nav_item, R.id.flContent);
+        // Add nav items
+        dlDrawer.addNavItem("First", R.drawable.ic_one, "First Fragment", FirstFragment.class);
+        dlDrawer.addNavItem("Second", R.drawable.ic_two, "Second Fragment", SecondFragment.class);
+        dlDrawer.addNavItem("Third", R.drawable.ic_three, "Third Fragment", ThirdFragment.class);
+        // Select default
+        if (savedInstanceState == null) {
+            dlDrawer.selectDrawerItem(0);   
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (dlDrawer.getDrawerToggle().onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        dlDrawer.getDrawerToggle().syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        dlDrawer.getDrawerToggle().onConfigurationChanged(newConfig);
+    }
+
+}
